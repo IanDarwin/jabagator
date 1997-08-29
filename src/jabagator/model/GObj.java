d780a614e05d6e1936936053dee45ed9e9aa80a2
IanDarwin <>
1997-08-29 20:19:28 +0000

    Substantial expansion, including contains(), doColor(), getParamString(), and selected/isSelected().

--- src/jabagator/model/GObj.java
+++ src/jabagator/model/GObj.java
@@ -1,7 +1,9 @@
 import java.awt.*;
 
 /**
- * List of Graphical Objects that JabaGator knows about 
+ * Top level of Graphical Objects that JabaGator knows about.
+ *
+ * Vaguely analogous to Component in java.awt.
  */
 public abstract class GObj extends Object {
 	/** The current position in units */
@@ -13,12 +15,50 @@ public abstract class GObj extends Object {
 	/** The current size in units */
 	int h;
 
+	static Component view;
+
+	static void setView(Component c) {
+		view = c;
+	}
+
 	GObj() {
+		if (view == null)
+			throw new IllegalArgumentException(
+				"Must setView() before constructing any GObj");
+
 		x = y = w = h = 0;
 	}
 
 	/** some methods... */
 	abstract void draw(Graphics g);
+
+	/** If this object is selected */
+	protected boolean selected;
+
+	public void setSelected(boolean b) {
+		selected = b;
+	}
+	public boolean isSelected() {
+		return selected;
+	}
+	void doColor(Graphics g) {
+		if (selected)
+			g.setColor(Color.blue);
+		else
+			g.setColor(Color.black);
+	}
+
+	/** Get the Parameter String (name+main values) of this GObj */
+	public String getParamString() {
+		return this+"[@"+x+","+y+";w="+w+",h="+h;
+	}
+
+	/** Return true if the object contains the point */
+	boolean contains(int cx, int cy) {
+		System.out.println(getParamString()+".contains("+cx+","+cy+")");
+		return (cx>=x && cx<=(x+w) &&
+				cy>=y && cy<=(y+h));
+	}
 
 	void setXY(int nx, int ny) {
 		x = nx;
