ae2ad84f8ad54573f1567ebf4ccd39b442593de0
IanDarwin <>
1997-08-29 20:24:26 +0000

    Add Polygon to fakeObj list. Add bogus delete() routine.

--- src/jabagator/model/JBModel.java
+++ src/jabagator/model/JBModel.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.*;
 
 /** This might someday be a draw program like Adobe Illustrator
@@ -10,8 +11,17 @@ public class JBModel {
 
 	JBModel() {
 		v = new Vector();
+	}
+
+	void delete() {
+		System.out.println("Would delete current object");
+	}
+
+	void fakeObjs() {
 	// highly fake:
-		GObj t = new GText();
+		GObj t;
+	// highly fake:
+		t = new GText();
 		((GText)t).setText("Hello");
 		t.setXY(100, 100);
 		v.addElement(t);
@@ -20,15 +30,28 @@ public class JBModel {
 		t.setXY(150, 200);
 		t.setWH(20,20);
 		v.addElement(t);
-
 	// highly fake:
 		t = new GOval();
 		t.setXY(50, 100);
-		t.setWH(20,20);
+		t.setWH(60,20);
 		v.addElement(t);
 	// highly fake:
 		t = new GRect();
 		t.setXY(250, 200);
+		t.setWH(20,40);
+		v.addElement(t);
+	// "This feelin' of fakin' it, of not really makin' it,
+	// I still haven't shaken it..."  -- simon & garfunkel
+		Polygon p = new Polygon();
+		p.addPoint(0,0);
+		p.addPoint(0, 50);
+		p.addPoint(50,0);
+		t = new GPoly(p);
+		t.setXY(300,70);
+		v.addElement(t);
+	// highly fake:
+		t = new GRect();
+		t.setXY(450, 300);
 		t.setWH(20,40);
 		v.addElement(t);
 	}
