16a751ba906c9a824d607ecbdc25b7410f8b82a2
IanDarwin <>
1997-08-29 20:22:16 +0000

    Add imports. Use FontMetrics to get width & height.

--- src/jabagator/model/GText.java
+++ src/jabagator/model/GText.java
@@ -1,12 +1,19 @@
-import java.awt.Graphics;
+import java.awt.*;
 
 public class GText extends GObj {
 	String text;
+	Font font;
 	void setText(String s) {
+		font = view.getFont();
 		text = s;
+		FontMetrics fm = view.getFontMetrics(font);
+		Dimension d = new Dimension(fm.stringWidth(text), fm.getAscent());
+		w = d.width; h = -(d.height);
 	}
 	void draw(Graphics g) {
-		System.out.println("Paint text " + text " + at + x + "," + y);
+		doColor(g);
+		// System.out.println("Paint text " + text + " at " + x + "," + y);
+		g.setFont(font);
 		g.drawString(text, x, y);
 	}
 }
