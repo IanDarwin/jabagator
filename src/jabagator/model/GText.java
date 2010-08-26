00536bf613331b30f888fd91443a3c2ee48e1be8
IanDarwin <>
2010-08-26 20:04:00 +0000

    Make things public; Represent indent on text bullets

--- src/jabagator/model/GText.java
+++ src/jabagator/model/GText.java
@@ -3,14 +3,17 @@ package jabagator.model;
 import java.awt.*;
 import javax.swing.*;
 
+/** Represents a text bullet */
 public class GText extends GObj {
 	String text;
 	Font font;
 	int tWidth, tHeight;
+	/** 0, 1, 2, 3 */
+	int indent;
 
 	public String describe() { return "text"; }
 
-	void setText(String s) {
+	public void setText(String s) {
 		font = getFont();
 		text = s;
 		FontMetrics fm = getFontMetrics(font);
@@ -20,6 +23,24 @@ public class GText extends GObj {
 		setSize(tWidth, tHeight);
 		ctlPoints[nCtlPoints++]=new Point(CP_SIZE/2, tHeight-CP_SIZE/2);
 	}
+
+	final int MAX_INDENT = 3;
+	public void setIndent(int indent) {
+		if (indent >= 0 && indent <= MAX_INDENT)
+			this.indent = indent;
+		else
+			throw new IllegalArgumentException("Indent must be in 0.." + MAX_INDENT);
+	}
+
+	public void indent() {
+		if (indent < MAX_INDENT)
+			indent++;
+	}
+	public void undent() {
+		if (indent > 0)
+			--indent;
+	}
+
 
 	/** draw -- common code for paint & print -- draw the object. */
 	protected void draw(Graphics g) {
