e9ff1a45d0499694b34a17cb85ccce65258c0684
IanDarwin <>
2011-10-15 21:36:32 +0000

    Trivial doc comments.

--- src/jabagator/io/LoadSave.java
+++ src/jabagator/io/LoadSave.java
@@ -4,8 +4,10 @@ import jabagator.model.JBModel;
 
 public interface LoadSave {
 
+	/** Convert FROM some external format TO our internal format, the JBModel */
 	public abstract void load(JBModel model);
 
+	/** Convert TO some external format FROM our internal format, the JBModel */
 	public abstract void save(JBModel model);
 
 }
